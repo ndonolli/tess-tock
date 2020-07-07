4b0102f971e763ed8546d23043f6a1e8e4eecc18
Nathan Donolli <ndonolli@gmail.com>
2020-07-07 02:45:28 -0500

    change to classes

--- public/index.js
+++ public/index.js
@@ -1,60 +1,83 @@
-const btn = document.querySelector('#main-btn')
-const AudioContext = window.AudioContext || window.webkitAudioContext;
+class Toggler  {
+  constructor() {
+    this.btn = document.querySelector('#main-btn');
+    this._muted = true;
+  }
 
-let audio = new AudioContext();
-let osc = audio.createOscillator();
-let lvl = audio.createGain();
-let muted = true;
-let started = false;
-let setOrientationPermission = false;
+  toggle = () => {
+    if (this._muted) {
+      this._muted = false;
+      this.btn.classList.add('active');
+      this.btn.innerHTML = 'Turn that shit off!';
 
-// methods
-// init Audiocontext
-const handler = () => {
-  const { beta } = event;
-  const ratio = (87 - beta) / 86
-  const fv = (55 + (1705 * ratio)); // 5 octaves between C1 and C6
-  osc.frequency.value = fv;
+    } else {
+      this._muted = true;
+      this.btn.classList.remove('active');
+      this.btn.innerHTML = 'Start';
+    }
+  }
+
+  get muted () {
+    return this._muted;
+  }
+
 }
 
-const init = () => {
-  window.addEventListener('deviceorientation', handler);
-  lvl.gain.value = 0;
-  osc.connect(lvl);
-  lvl.connect(audio.destination);
-  osc.start(0);
-  started = true;
-}
+class Theremin {
+  constructor() {
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    this.audio = new AudioContext();
+    this.osc = this.audio.createOscillator();
+    this.lvl = this.audio.createGain();
+  }
 
-const toggleSound = () => {
-  if (!started) {
-    if (typeof DeviceOrientationEvent.requestPermission === 'function' 
-        && !setOrientationPermission) {
+
+  _handler() {
+    const { beta } = event;
+    const ratio = (87 - beta) / 86
+    const fv = (55 + (1705 * ratio)); // 5 octaves between C1 and C6
+    this.osc.frequency.value = fv;
+  }
+
+  init() {
+    this.lvl.gain.value = 1;
+    this.osc.connect(this.lvl);
+    this.osc.start(0);
+  }
+
+  start() {
+    this.lvl.connect(this.audio.destination);
+  }
+
+  stop() {
+    this.lvl.disconnect(this.audio.destination);
+  }
+
+  register = () => {
+    if (window.DeviceOrientationEvent && typeof DeviceOrientationEvent.requestPermission === 'function') {
       DeviceOrientationEvent.requestPermission().then(response => {
         if (response == 'granted') {
-          window.addEventListener('deviceorientation', handler);
-          setOrientationPermission = true;
+          window.addEventListener('deviceorientation', this._handler.bind(this));
         }
       }).catch(console.error)
     } else {
-      init();
-      toggleSound();
-    }
-
-  } else {
-    if (muted) {
-      lvl.gain.value = 1;
-      muted = false;
-      btn.classList.add('active');
-      btn.innerHTML = 'Turn that shit off!';
-
-    } else {
-      lvl.gain.value = 0;
-      muted = true;
-      btn.classList.remove('active');
-      btn.innerHTML = 'Start';
+      window.addEventListener('deviceorientation', this._handler.bind(this));
     }
   }
 }
 
-window.toggleSound = toggleSound;
+let theremin;
+let toggler = new Toggler();
+
+const clickHandler = () => {
+  if (!theremin) {
+    theremin = new Theremin();
+    theremin.init();
+    theremin.register();
+  }
+  toggler.muted ? theremin.start() : theremin.stop();
+  toggler.toggle();
+
+}
+
+window.clickHandler = clickHandler;
